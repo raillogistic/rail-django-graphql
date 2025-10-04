56eb0604372d4c648148a0961d515d3e3b100f0d
it@rail-logistic.dz <it@rail-logistic.dz>
2025-10-04 17:50:49 +0100

    chore(repo): move repository root to library folder and reindex

--- django-graphql-boilerplate/config/schema.py
+++
@@ -1,8 +0,0 @@
-"""
-Main GraphQL schema for django-graphql-boilerplate project.
-"""
-
-from rail_django_graphql.core.schema import get_schema as get_auto_schema
-
-# Return the auto-generated GraphQL schema managed by the library
-schema = get_auto_schema(schema_name="default")
--- django-graphql-boilerplate/docs/advanced/index.md
+++
@@ -1,5 +0,0 @@
-# Advanced Topics
-
-- Authentication and authorization strategies
-- Caching and invalidation
-- Performance middleware metrics
